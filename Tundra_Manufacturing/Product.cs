38f56a49c60ac6fdfe0c0ab6335cd2485b2173a4
Michael Lukatchik <mlukatchik@unitedoneresources.com>
2016-10-18 22:08:55 -0400

    first commit

---
+++ Tundra_Manufacturing/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tundra_Manufacturing
+{
+    public class Product
+    {
+        public string ProductDescription { get; set; }
+        public string InventoryCode { get; set; }
+        public DateTime? DateAvailable { get; set; }
+        public decimal? CostPerUnit { get; set; }
+        public int? Quantity { get; set; }
+        public string Comments { get; set; }
+    }
+}
---
+++ Tundra_Manufacturing/SpreadsheetReader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using LinqToExcel;
+
+namespace Tundra_Manufacturing
+{
+    public class SpreadsheetReader
+    {
+        public void ReadSpreadsheet()
+        {
+            // Set up the path to the spreadsheet to be read
+            var excelQueryFactory = new ExcelQueryFactory("C:/users/mlukatchik/desktop/Tundra_Manufacturing.xlsx");
+
+            // Define column mappings
+            excelQueryFactory.AddMapping<Product>(x => x.ProductDescription, "Product");
+            excelQueryFactory.AddMapping<Product>(x => x.InventoryCode, "Inventory Code");
+            excelQueryFactory.AddMapping<Product>(x => x.DateAvailable, "Date Available");
+            excelQueryFactory.AddMapping<Product>(x => x.CostPerUnit, "Cost Per Unit");
+            excelQueryFactory.AddMapping<Product>(x => x.Quantity, "Quantity");
+            excelQueryFactory.AddMapping<Product>(x => x.Comments, "Comments");
+
+            // Read the data
+            var lineItems = from oli in excelQueryFactory.Worksheet<Product>("Sheet1") select oli;
+
+            // Do something with the data
+            foreach (var lineItem in lineItems)
+            {
+                //Note that you have access to each property on the object
+                Console.WriteLine(lineItem.Comments);
+            }
+
+        }
+    }
+}
